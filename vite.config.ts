b113785d0188f156085cfbeaa3779c201bed963d
HassanTahir14 <hassan.tahir@exirodigital.com>
2025-09-01 02:24:04 +0300

    vercel configurations

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000", 
-        changeOrigin: true,
-      },
-    },
-  },
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
